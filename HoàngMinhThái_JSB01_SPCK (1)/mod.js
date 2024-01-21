5cf856ef5c4cfd8bd34999623c5744ba7a5d482e
red-bananana <148669564+Red-bananana@users.noreply.github.com>
2024-01-21 10:37:39 +0700

    Add files via upload

---
+++ HoàngMinhThái_JSB01_SPCK (1)/mod.js
@@ -0,0 +1,12 @@
+let addcart = document.querySelector(".addcart-btn")
+let cart = document.querySelector("#goodscount")
+let count = 0
+cart.innerHTML = count
+addcart.addEventListener("click", (e)=> {
+        e.preventDefault()  
+        count++
+    })
+// addcart.addEventListener("click", (e)=> {
+//     e.preventDefault()  
+//     count++
+// })
